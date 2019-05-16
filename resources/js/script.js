4c9f27175457df8f0008654df25dd1946d0a7f37
ciacono <claire.iacono@gmail.com>
2019-05-16 14:37:37 -0700

    added basic js functionlity

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,24 @@
-//insert js here
+function submitMessage() {
+  var message = document.getElementById('newMessage').value;
+  var newItem = document.createElement('li');
+  newItem.innerHTML = message;
+  document.getElementById('messageList').appendChild(newItem);
+}
+function clearMessage() {
+  //stub
+}
+
+
+function makeList() {
+  var array = ['Message 1', 'Message 2', 'Message 3'];
+  for (var i = 0; i < array.length; i++) {
+    var message = document.createElement('li');
+    message.innerHTML = array[i];
+    document.getElementById('messageList').appendChild(message);
+  }
+}
+window.onload = makeList;
+
+function clearList() {
+  //stub
+}
